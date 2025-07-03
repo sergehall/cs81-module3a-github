fb97abf32645e2f5ff7bb70fd862b7112eabed15
serge <serge.hall.dev@gmail.com>
2025-07-03 00:25:16 -0700

    Add factorial() function

--- journal.js
+++ journal.js
@@ -17,3 +17,9 @@ function square(num) {
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
+
+// Get factorial
+function factorial(n) {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+}
